e6d555b75f6c55d6d91b13d21a499dbbf088f685
wasu <wevorence@gmail.com>
2018-08-08 15:41:55 +0000

    By the way, I did not implement a password for user :D

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create()
+    User.create(user_params)
+    redirect_to '/'
+  end
+  def show()
+    @users = User.all
+    @user = User.new
+    render :user
+  end
+  def delete()
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :bio)
+  end
 end
